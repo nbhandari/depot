f980384ec0875341d2da8e8157f69d52795d56ca
Nitin Bhandari <nitin.bhandari@gmail.com>
2009-01-27 23:47:41 -0800

    Tests Passing

---
+++ app/models/product.rb
@@ -0,0 +1,19 @@
+class Product < ActiveRecord::Base
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price
+  validate  :price_must_be_at_least_a_cent
+  validates_uniqueness_of :title
+  validates_format_of :image_url,
+                             :with => /\.(gif|jpg|png)/i ,
+                             :message => 'must be a url for image'
+  protected
+  def price_must_be_at_least_a_cent
+    errors.add(:price, 'should be atleast 0.01') if price.nil? ||
+      price < 0.01
+  end
+  
+  def self.find_products_for_sale
+    find(:all, :order => "title")
+  end
+
+end
