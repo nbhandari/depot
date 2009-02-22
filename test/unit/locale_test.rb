4820416c1b891dcbbb53fe894f04b3941b6c8f2c
Nitin Bhandari <nitin.bhandari@gmail.com>
2009-02-22 14:45:00 -0800

    Tests for Translations

---
+++ test/unit/locale_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class LocaleTest < ActiveSupport::TestCase
+  
+  test "english page title" do
+    locale = 'en'
+    I18n.locale = locale
+    
+    assert_equal "MY DEPOT", I18n.t('layout.title')
+  end
+  
+  test "english order prompt" do
+    locale = 'en'
+    I18n.locale = locale
+    
+    assert_equal "Please select a type", I18n.t('order.prompt')
+  end
+
+  test "english order paytype po" do
+    locale = 'en'
+    I18n.locale = locale
+    
+    assert_equal "Purchase Order", I18n.t('order.paytype.po')
+  end
+  
+  test "hindi page title" do
+    locale = 'hi'
+    I18n.locale = locale
+    
+    assert_equal "Meri Dukaan", I18n.t('layout.title')
+  end
+end
